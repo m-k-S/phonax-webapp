e80f5de1671f1528d957873a30bab9c77ab28034
Max Shirokawa <muon@alchemist.local>
2024-08-26 14:38:21 -0400

    code cleaning

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ from flask import Flask, request, render_template, send_file, Response, url_for
 from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, emit
 
-import io
-import base64
-from threading import Thread
-
 from pymatgen.core import Structure
 from pymatgen.io.cif import CifParser
 from pymatgen.io.ase import AseAtomsAdaptor
@@ -26,9 +22,6 @@ from phonax.phonons import (
     plot_phonon_DOS,
     dynamical_matrix
 )
-
-from matplotlib.backends.backend_svg import FigureCanvasSVG
-from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 from plotting import plot_bands, mpl_to_html, render_mpl
 from tests.test_model import test_hessian
@@ -59,30 +52,6 @@ def task():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Source:
-    # https://matplotlib.org/gallery/lines_bars_and_markers/simple_plot.html
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    t = np.arange(0.0, 2.0, 0.01)
-    s = 1 + np.sin(2 * np.pi * t)
-
-    fig, ax = plt.subplots()  
-    ax.plot(t, s)  
-    ax.set(
-        xlabel='time (s)',
-        ylabel='voltage (mV)',  
-        title='About as simple as it gets, folks'
-    )
-
-    # Add file upload form to the HTML
-    upload_form = '''
-        <form method="post" enctype="multipart/form-data">
-            <input type="file" name="file">
-            <input type="submit" value="Upload and Process">
-        </form>
-    '''
-
     # Handle file upload and processing
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -119,40 +88,18 @@ def index():
 
             print ('predicting hessian')
 
-            # H = test_hessian(graph)
-            H = predict_hessian_matrix(params,model_fn,graph)
+            H = test_hessian(graph)
+            # H = predict_hessian_matrix(params,model_fn,graph)
 
             fig = plot_bands(atoms, graph, H, npoints=1000)
             fig_html = mpl_to_html(fig)
+
+            event_emitter.emit('complete', {'plot_data': fig_html})
 
             return render_template('index.html', fig=fig_html)
         
     # If it's a GET request, just show the upload form
     return render_template('index.html')
-
-@app.route("/phonon-plot.svg")
-def plot_svg():
-    """ renders the plot on the fly.
-    """
-
-    fig = process_file('SiO2.cif')
-
-    output = io.BytesIO()
-    FigureCanvasSVG(fig).print_svg(output)
-    return Response(output.getvalue(), mimetype="image/svg+xml")
-
-@app.route("/phonon.png")
-def plot_png(num_x_points=50):
-    """ renders the plot on the fly.
-    """
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    x_points = range(num_x_points)
-    axis.plot(x_points, [np.random.randint(1, 30) for x in x_points])
-
-    output = io.BytesIO()
-    FigureCanvasAgg(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
 
 @socketio.on('connect')
 def test_connect():
